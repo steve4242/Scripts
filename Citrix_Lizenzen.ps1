eb4d1dd954a327ca2efae3e82c97b48fd299d643
Stefan Biedermann <s.biedermann@salesianer.com>
2021-06-22 08:25:56 +0200

    anpassung

--- Citrix_Lizenzen.ps1
+++ Citrix_Lizenzen.ps1
@@ -1,3 +1,7 @@
+# beim .csv file muss jedes Monat das Datum angepasst werden z.B.: license_report_2021-06.csv --> license_report_2021-07.csv
 
+#Import der CSV
 $file = Import-Csv \\salctxfil1.salres.com\citrix$\DataTransfer\Licensing\license_report_2021-06.csv -Header "DateTime", "Installed", "InUse", "Overdraft", "UsagePercent" -Delimiter ";"
+
+# Letzte Zeile ausgeben
 $file | Select-Object -Last 1
